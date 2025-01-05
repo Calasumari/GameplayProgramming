e564f547387686368d73d3f4f5c5c8f9c1467b0f
Calasumari <mollyanne900@gmail.com>
2025-01-05 21:43:51 +0000

    Art

--- Assets/Scripts/Fun Clothes.cs
+++ Assets/Scripts/Fun Clothes.cs
@@ -11,25 +11,25 @@ public class FunClothes : MonoBehaviour
     public string Name;
     public string Type;
 
+    // started as seperate scripts for every clothing but the maths manager means i can reduce it to three - one for each type
+    // all the code already uses "cute clothing" in scripts so i won't change the name
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        //if signal received Score Caculation && position == (Wherever dog is) 
-        // Final Points return
-        
-    }
+
 
     public double PointsCalculation()
     {
-        double Cute = CutePoints * 0.2;
+        //might bit be needed, here as backup only
+        double Cute = CutePoints * 1.2;
         double Cool = CoolPoints * 0.2;
-        double Fun = FunPoints * 1.2;
+        double Fun = FunPoints * 0.2;
         double Smart = SmartPoints * 0.2;
 
         return (Cute + Cool + Fun + Smart);
